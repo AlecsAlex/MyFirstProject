db4a138a29b9652549b6e07a03015ef5d3c3e4db
AlecsAlex <alecsandru.fordea@gmail.com>
2023-09-10 22:06:34 +0300

    100923

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -87,17 +87,72 @@ plt.legend()
 
 label = "squat"
 participant = "A"
-all_axis_df = df.query(f"label == '{label}'").query(f"participant == '{participant}'").reset_index()
+all_axis_df = (
+    df.query(f"label == '{label}'")
+    .query(f"participant == '{participant}'")
+    .reset_index()
+)
+
+
+fig, ax = plt.subplots()
+all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax)
+ax.set_ylabel("acc_y")
+ax.set_xlabel("samples")
+plt.legend()
 
 
 # --------------------------------------------------------------
 # Create a loop to plot all combinations per sensor
 # --------------------------------------------------------------
 
+labels = df["label"].unique()
+participants = df["participant"].unique()
+
+for label in labels:
+    for participant in participants:
+        all_axis_df = (
+            df.query(f"label == '{label}'")
+            .query(f"participant == '{participant}'")
+            .reset_index()
+        )
+        if len(all_axis_df) > 0:
+            fig, ax = plt.subplots()
+            all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax)
+            ax.set_ylabel("acc_y")
+            ax.set_xlabel("samples")
+            plt.title(f"{label} ({participant})".title())
+            plt.legend()
+
+
+for label in labels:
+    for participant in participants:
+        all_axis_df = (
+            df.query(f"label == '{label}'")
+            .query(f"participant == '{participant}'")
+            .reset_index()
+        )
+        if len(all_axis_df) > 0:
+            fig, ax = plt.subplots()
+            all_axis_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax)
+            ax.set_ylabel("gyr_y")
+            ax.set_xlabel("samples")
+            plt.title(f"{label} ({participant})".title())
+            plt.legend()
+
 
 # --------------------------------------------------------------
 # Combine plots in one figure
 # --------------------------------------------------------------
+
+label = "row"
+participant = "A"
+combined_plot_df = (
+    df.query(f"label == '{label}'")
+    .query(f"participant == '{participant}'")
+    .reset_index()
+)
+
+
 
 
 # --------------------------------------------------------------
