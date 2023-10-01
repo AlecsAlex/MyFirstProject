cb7ea5e7733097dee8d939d4a2790affb9d7a518
AlecsAlex <alecsandru.fordea@gmail.com>
2023-10-01 14:40:44 +0300

    011023_1440

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from IPython.display import display
@@ -152,10 +153,48 @@ combined_plot_df = (
     .reset_index(drop=True)
 )
 
-fig, ax = plt.subplots(nrows=2, sharex = True, figsize = (20,10))
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
 combined_plot_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax[0])
 combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+
+
+ax[0].legend(
+    loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True
+)
+ax[1].legend(
+    loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True
+)
+ax[1].set_xlabel("samples")
+
 
 # --------------------------------------------------------------
 # Loop over all combinations and export for both sensors
 # --------------------------------------------------------------
+
+
+labels = df["label"].unique()
+participants = df["participant"].unique()
+
+for label in labels:
+    for participant in participants:
+        all_axis_df = (
+            df.query(f"label == '{label}'")
+            .query(f"participant == '{participant}'")
+            .reset_index()
+        )
+        if len(combined_plot_df) > 0:
+            fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+            combined_plot_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax[0])
+            combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+
+
+            ax[0].legend(
+                loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True
+            )
+            ax[1].legend(
+                loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True
+            )
+            ax[1].set_xlabel("samples")
+            
+            plt.savefig(f"../../reports/figures/{label.title()} ({participant}).png")
+            plt.show()
